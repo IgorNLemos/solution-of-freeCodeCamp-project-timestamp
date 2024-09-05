1f63d4f229f07817494822663cf4b83c2510923e
IgorNLemos <147350759+IgorNLemos@users.noreply.github.com>
2024-09-05 02:57:28 +0000

    Tested solution of time tracker

--- index.js
+++ index.js
@@ -19,9 +19,23 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", (req, res) => {
+  let date;
+  
+  if (!req.params.date) {
+    date = new Date();
+  } else {
+    date = new Date(isNaN(req.params.date) ? req.params.date : +req.params.date);
+  }
+
+  if (date.toString() === 'Invalid Date') {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  }
 });
 
 
